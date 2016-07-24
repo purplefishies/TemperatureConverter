0a0d20196cd72c1497bdb4cfc2a61eaca968be49
Jimi Damon <jdamon@gmail.com>
2016-07-23 22:29:02 -0700

    ENH: added junit and core library dependency
    - needed to add import org.junit.Test;
    - add then the @Test
    - File -> new module , then add the library

--- app/src/androidTest/java/com/accesio/temperatureconverter/ApplicationTest.java
+++ app/src/androidTest/java/com/accesio/temperatureconverter/ApplicationTest.java
@@ -2,7 +2,7 @@ package com.accesio.temperatureconverter;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
---
+++ core/src/main/java/com/example/MyClass.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class MyClass {
+
+    private int number;
+    private String name;
+
+    public MyClass(int number, String name ) {
+        this.number = number;
+        this.name = name;
+    }
+}
